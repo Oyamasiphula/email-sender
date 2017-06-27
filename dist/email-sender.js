9936051a835aa92dd2dff3122d8c26c0bbb1c5a0
bootcamp <myezasifiso11@gmail.com>
2017-06-27 15:13:11 +0200

    create mailInterface, and sendfunction

---
+++ dist/email-sender.js
@@ -0,0 +1,3 @@
+"use strict";
+function emailSender(EmailData) {
+}
---
+++ src/email-sender.ts
@@ -0,0 +1,12 @@
+
+interface emailData {
+  coderName : String
+  email:String
+  subject: String
+  templateName:String
+  feedBackSeason:String
+}
+
+function emailSender(EmailData:emailData){
+  
+}
