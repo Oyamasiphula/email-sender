25a8f70cbd3e96c8fb4566874cf26d58beb27a08
bootcamp <myezasifiso11@gmail.com>
2017-06-27 16:00:03 +0200

    create function that returns Hi codername

--- dist/email-sender.js
+++ dist/email-sender.js
@@ -1,3 +1,5 @@
 "use strict";
-function emailSender(EmailData) {
+console.log(emailSender({ coderName: "Sive", email: "sive@gmail.com", subject: "Week 1 feedback", templateName: "notificationToCoder", feedBackSeason: "set1" }));
+function emailSender(emailData) {
+    return "Hi " + emailData.coderName;
 }
--- src/email-sender.ts
+++ src/email-sender.ts
@@ -1,12 +1,13 @@
-
-interface emailData {
+ // import Module from 'module'
+interface EmailData {
   coderName : String
   email:String
   subject: String
   templateName:String
   feedBackSeason:String
 }
+console.log(emailSender({coderName:"Sive",email:"sive@gmail.com",subject:"Week 1 feedback", templateName: "notificationToCoder",feedBackSeason:"set1"}))
 
-function emailSender(EmailData:emailData){
-  
+function emailSender(emailData:EmailData){
+  return "Hi " + emailData.coderName;
 }
