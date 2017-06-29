50e8403942849c18c56b84572025db245debddcb
oyamasiphula <oyama@projectcodex.co>
2017-06-29 16:38:15 +0200

    added function emailsender with more params to use from an existing object

--- src/nodemailer-tests.ts
+++ src/nodemailer-tests.ts
@@ -1,36 +1,37 @@
 
 import * as nodemailer from 'nodemailer'
 
-// create reusable transporter object using SMTP transport
-var transporter: nodemailer.Transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: process.env.GMUNAME,
-        pass: process.env.GMPASS
-    }
-});
+var sendMail = (email: String, subject: String, feedBackSeason: String, ): void => {
+    // create reusable transporter object using SMTP transport
+    var transporter: nodemailer.Transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: process.env.GMUNAME,
+            pass: process.env.GMPASS
+        }
+    });
+    // create reusable transporter object using SMTP transport and set default values for mail options.
+    transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: process.env.GMUNAME,
+            pass: process.env.GMPASS
+        }
+    });
+    // setup e-mail data with unicode symbols
+    var mailOptions: nodemailer.SendMailOptions = {
+        from: 'Fred Foo ✔ <oyamasiphula@gmail.com>', // sender address
+        to: email, // list of receivers
+        subject: subject + '✔', // Subject line
+        text: feedBackSeason + '✔', // plaintext body
+        html: "html" + '<b>Hello world again✔</b>' // html body
+    };
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error: Error, info: nodemailer.SentMessageInfo): void => {
+        //    displaying an error
+        console.log(error);
+        //Then nothing else
+    });
+}
 
-// create reusable transporter object using SMTP transport and set default values for mail options.
-transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: process.env.GMUNAME,
-        pass: process.env.GMPASS
-    }
-});
-
-// setup e-mail data with unicode symbols
-var mailOptions: nodemailer.SendMailOptions = {
-    from: 'Fred Foo ✔ <oyamasiphula@gmail.com>', // sender address
-    to: 'onwa.mrhwetyana@gmail.com', // list of receivers
-    subject: "subject" + 'Hello again✔', // Subject line
-    text: "text" + 'Hello world again ✔', // plaintext body
-    html: "html" + '<b>Hello world again✔</b>' // html body
-};
-
-// send mail with defined transport object
-transporter.sendMail(mailOptions, (error: Error, info: nodemailer.SentMessageInfo): void => {
-
-  console.log(error);
-	// nothing
-});
+exports.sendMail = sendMail;
