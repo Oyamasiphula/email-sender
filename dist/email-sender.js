9476ed994f25b584d52ffb7c6b7fb4c33d3cf0d9
oyamasiphula <oyama@projectcodex.co>
2017-06-29 16:38:15 +0200

    added function within an object

--- dist/email-sender.js
+++ dist/email-sender.js
@@ -5,6 +5,8 @@ var fs = require("fs");
 var handlebars = require("handlebars");
 createMsg({ coderName: "Sive", email: "sive@gmail.com", subject: "Week 1 feedback", templateName: "notification-1.handlebars", feedBackSeason: "set1" });
 function createMsg(emailData) {
+    var sendMail = { emailSender: String };
+    console.log(sendMail);
     var template = fs.readFileSync('./templates/' + emailData.templateName, 'utf-8');
     var source = handlebars.compile(template);
     //  console.log(template)
